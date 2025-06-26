7ce3bf70980278f2793ecb4316ddd754fee4f51a
권장순 <jsoonworld@gmail.com>
2025-06-26 15:09:08 +0900

    feat(Basic Auth) : Basic Auth 구현

--- spring-auth-1/initial/src/main/java/cholog/auth/ui/SessionLoginController.java
+++ spring-auth-1/initial/src/main/java/cholog/auth/ui/SessionLoginController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
 public class SessionLoginController {
     private static final String SESSION_KEY = "USER";
